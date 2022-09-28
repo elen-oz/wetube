4387d6f73d41769e0e0123831608f9561f43261a
Elena Ozerova <ozerova.lenka@gmail.com>
2022-09-28 12:34:30 +0200

    init: start project

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>I am a header</h1>
+      <MenuSharpIcon />
+    </div>
+  )
+}
+
+export default Header
