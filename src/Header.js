c219fe5f85d6d8b750f678d24012ae4503bb564d
Elena Ozerova <ozerova.lenka@gmail.com>
2022-09-28 14:00:24 +0200

    feat: add icons in header

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+import React from "react";
+import MenuSharpIcon from "@mui/icons-material/MenuSharp";
+import logo from "./logo.svg";
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import MicOutlinedIcon from '@mui/icons-material/MicOutlined';
+import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
+import AppsSharpIcon from '@mui/icons-material/AppsSharp';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import Avatar from '@mui/material/Avatar';
+
 
 function Header() {
   return (
-    <div className="header">
-      <h1>I am a header</h1>
+    <div className='header'>
       <MenuSharpIcon />
+      <img 
+        className="header__logo"
+        src={logo}
+        alt="logo"
+      />
+      <input type="text" />
+      <SearchSharpIcon />
+      <MicOutlinedIcon />
+      <VideoCallOutlinedIcon />
+      <AppsSharpIcon />
+      <NotificationsOutlinedIcon />
+      <Avatar>I</Avatar>
+      {/* <button></button> */}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
