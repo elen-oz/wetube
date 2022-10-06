92af4545876aa36f1ed374807434767530e40847
Elena Ozerova <ozerova.lenka@gmail.com>
2022-10-06 21:02:23 +0200

    refactor: optimize data sharing

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "../styles/MainSection.css";
-import VideoCard from "./VideoCard";
+// import VideoCard from "./VideoCard";
+import { getDataVideoCard } from "../utilities";
 
 function MainSection({ videosData }) {
-  const videosComponents = [];
+  // const videosComponents = [];
 
-  for (let i = 0; i < videosData.length; i += 1) {
-    const video = videosData[i];
+  // for (let i = 0; i < videosData.length; i += 1) {
+  //   const video = videosData[i];
 
-    videosComponents.push(
-      <VideoCard
-        id={video.id}
-        videoId={video.videoId}
-        videoThumbnail={video.videoThumbnail}
-        videoName={video.videoName}
-        channelName={video.channelName}
-        views={video.views}
-        channelThumbnail={video.channelThumbnail}
-      />
-    );
-  }
+  //   videosComponents.push(
+  //     <VideoCard
+  //       id={video.id}
+  //       videoId={video.videoId}
+  //       videoThumbnail={video.videoThumbnail}
+  //       videoName={video.videoName}
+  //       channelName={video.channelName}
+  //       views={video.views}
+  //       channelThumbnail={video.channelThumbnail}
+  //     />
+  //   );
+  // }
+
+  const videosComponents = getDataVideoCard(videosData);
 
   return (
     <div className='mainSection'>
--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,32 @@
+import React from "react";
+import VideoCard from "./components/VideoCard";
+
+export const getDataVideoCard = (videosData) => {
+  const videosComponents = [];
+
+  for (let i = 0; i < videosData.length; i += 1) {
+    const video = videosData[i];
+
+    videosComponents.push(
+      <VideoCard
+        id={video.id}
+        videoId={video.videoId}
+        videoThumbnail={video.videoThumbnail}
+        videoName={video.videoName}
+        channelName={video.channelName}
+        views={video.views}
+        channelThumbnail={video.channelThumbnail}
+      />
+    );
+  }
+
+  return videosComponents;
+};
+
 export const getFormatCheck = (fileName) => {
-  if (!(fileName.endsWith(".jpg"))) {
+  if (!fileName.endsWith(".jpg")) {
     fileName = `${fileName}.jpg`;
-  } 
+  }
 
   return fileName;
 };
@@ -9,17 +34,15 @@ export const getFormatCheck = (fileName) => {
 export const VideoIDToEmbed = (videoAddress) => {
   videoAddress = videoAddress.split("?v=");
   let correctAddress = `https://www.youtube.com/embed/${videoAddress[1]}`;
-  let result = correctAddress.replace(' ', '');
-  
+  let result = correctAddress.replace(" ", "");
+
   return result;
 };
-
 
 export const SearchForVideo = ({ videosData, searchTerm }) => {
   const lowerCasedSearchTerm = searchTerm.toLowerCase();
 
-  return videosData.filter((video) => 
+  return videosData.filter((video) =>
     video.videoName.toLowerCase().includes(lowerCasedSearchTerm)
-  )
+  );
 };
-
