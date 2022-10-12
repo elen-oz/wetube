b27741db4fad1fd8e48d1e46261ceac5f6ee5b6f
Elena Ozerova <ozerova.lenka@gmail.com>
2022-10-12 18:51:27 +0200

    feat: add search by enter

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,32 +14,52 @@ function Header() {
   return (
     <div className='header'>
       <div className='header__left'>
-        <MenuSharpIcon />        
+        <MenuSharpIcon />
         <Link to='/'>
           <img className='header__logo' src={logo} alt='wetube logo' />
         </Link>
       </div>
 
-      <div className='header__input'>
-        <input
-          onChange={(e) => setInputSearch(e.target.value)}
-          value={inputSearch}
-          placeholder='Search'
-          type='text'
-        />
-        <Link to={`/search/${inputSearch}`}>
-          <SearchSharpIcon className='header__searchButton' />
-        </Link>
+      <div>
+        <form  className='header__input-form'>
+          <input
+            onChange={(e) => setInputSearch(e.target.value)}
+            value={inputSearch}
+            placeholder='Search'
+            type='text'
+          />
+
+          <Link to={`/search/${inputSearch}`}>
+            <button className='header__searchButton' type='submit'>
+              <SearchSharpIcon className='header__searchButton-icon' />
+            </button>
+          </Link>
+        </form>
       </div>
 
       <div className='header__icons'>
-        <a className='text' target='_blank' rel="noopener noreferrer" href='https://github.com/elen-oz'>
+        <a
+          className='text'
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://github.com/elen-oz'
+        >
           <VideoCallOutlinedIcon className='header__icon' />
         </a>
-        <a className='text' target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/elen-oz/'>
+        <a
+          className='text'
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://www.linkedin.com/in/elen-oz/'
+        >
           <NotificationsOutlinedIcon className='header__icon' />
         </a>
-        <a className='text' target='_blank' rel="noopener noreferrer" href='https://www.instagram.com/lenka_oz/'>
+        <a
+          className='text'
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://www.instagram.com/lenka_oz/'
+        >
           <Avatar sx={{ width: 28, height: 28 }} className='header__icon'>
             I
           </Avatar>
